c56de24264e27a722f3ac6828215ad1db22f0397
Alexara Wu <alexarawu@outlook.com>
2022-12-27 18:17:09 +0800

    feat: add tds file locations

--- sjtutex/build.lua
+++ sjtutex/build.lua
@@ -18,3 +18,9 @@ unpackexe          = "xetex"
 unpackfiles        = {"sjtutex.dtx"}
 
 packtdszip         = true
+
+tdslocations = {
+  "tex/latex/sjtutex/config/*.cfg",
+  "tex/latex/sjtutex/fd/sjtu-*-font-*.def",
+  "tex/latex/sjtutex/vi/sjtu-vi-*.pdf"
+}
