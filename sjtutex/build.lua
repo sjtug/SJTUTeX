855010b1c1dfa4285d63563e56967726149793d1
Alexara Wu <alexarawu@outlook.com>
2025-01-07 18:16:08 +0800

    refactor: rearrange code

--- sjtutex/build.lua
+++ sjtutex/build.lua
@@ -31,8 +31,8 @@ packtdszip         = true
 tdslocations = {
   "tex/latex/sjtutex/font/sjtu-*-font-*.def",
   "tex/latex/sjtutex/lang/sjtu-lang-*.def",
-  "tex/latex/sjtutex/name/sjtu-name-*.def",
   "tex/latex/sjtutex/scheme/sjtu-scheme-*.def",
+  "tex/latex/sjtutex/thesis/sjtu-thesis-*.def",
   "tex/latex/sjtutex/vi/sjtu-vi-*.pdf"
 }
 
